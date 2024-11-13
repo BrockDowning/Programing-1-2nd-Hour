8c1da1c81d0663514415d5583e7143077d882dcf
BrockDowning <downing.b@stu.janesville.k12.wi.us>
2024-11-13 09:39:55 -0600

    11/13/24

--- Prog122i/Prog122i/MainForm.py
+++ Prog122i/Prog122i/MainForm.py
@@ -1,6 +1,6 @@
-﻿import System.Drawing
+﻿import math
+import System.Drawing
 import System.Windows.Forms
-
 from System.Drawing import *
 from System.Windows.Forms import *
 
@@ -17,20 +17,20 @@ class MainForm(Form):
         # 
         # listBox1
         # 
-        self._listBox1.BackColor = System.Drawing.Color.FromArgb(255, 255, 128)
+        self._listBox1.BackColor = System.Drawing.Color.FromArgb(128, 255, 255)
         self._listBox1.FormattingEnabled = True
-        self._listBox1.Location = System.Drawing.Point(162, 12)
+        self._listBox1.Location = System.Drawing.Point(12, 71)
         self._listBox1.Name = "listBox1"
-        self._listBox1.Size = System.Drawing.Size(515, 420)
+        self._listBox1.Size = System.Drawing.Size(763, 368)
         self._listBox1.TabIndex = 0
         # 
         # button1
         # 
-        self._button1.BackColor = System.Drawing.Color.FromArgb(255, 192, 192)
+        self._button1.BackColor = System.Drawing.Color.LightSkyBlue
         self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
         self._button1.Location = System.Drawing.Point(13, 13)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(143, 138)
+        self._button1.Size = System.Drawing.Size(263, 52)
         self._button1.TabIndex = 1
         self._button1.Text = "Calculate"
         self._button1.UseVisualStyleBackColor = False
@@ -38,11 +38,11 @@ class MainForm(Form):
         # 
         # button2
         # 
-        self._button2.BackColor = System.Drawing.Color.FromArgb(255, 192, 192)
+        self._button2.BackColor = System.Drawing.Color.LightSkyBlue
         self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-        self._button2.Location = System.Drawing.Point(13, 157)
+        self._button2.Location = System.Drawing.Point(282, 12)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(143, 138)
+        self._button2.Size = System.Drawing.Size(224, 52)
         self._button2.TabIndex = 2
         self._button2.Text = "Clear"
         self._button2.UseVisualStyleBackColor = False
@@ -50,11 +50,11 @@ class MainForm(Form):
         # 
         # button3
         # 
-        self._button3.BackColor = System.Drawing.Color.FromArgb(255, 192, 192)
+        self._button3.BackColor = System.Drawing.Color.LightSkyBlue
         self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-        self._button3.Location = System.Drawing.Point(12, 299)
+        self._button3.Location = System.Drawing.Point(512, 12)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(143, 138)
+        self._button3.Size = System.Drawing.Size(263, 52)
         self._button3.TabIndex = 3
         self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = False
@@ -62,27 +62,22 @@ class MainForm(Form):
         # 
         # MainForm
         # 
-        self.BackColor = System.Drawing.Color.FromArgb(255, 192, 128)
-        self.ClientSize = System.Drawing.Size(689, 449)
+        self.BackColor = System.Drawing.Color.Navy
+        self.ClientSize = System.Drawing.Size(787, 458)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Controls.Add(self._listBox1)
         self.Name = "MainForm"
-        self.Text = "Prog122d"
+        self.Text = "Prog122i"
         self.ResumeLayout(False)
 
 
     def Button1Click(self, sender, e):
-        for num in range(-12, 17):
-            sixth  = num**6
-            fifth  = 3 * (num)**5
-            fourth = 93 * (num)**4
-            third  = 87 * (num)**3
-            second = 1596 * (num)**2
-            first  = 1380 * (num)
-            formula = sixth - fifth - fourth + third + second - first - 2800 
-            list = str(num) + "\t\t" + str(formula)
+        for num in range(-25, 26):
+            cubed = (num**2) * num
+            CubeR = num**1/3.00
+            list = str(num) + "\t\t" + str(CubeR) + "\t\t" + str(cubed)
             self._listBox1.Items.Add(list)
 
     def Button2Click(self, sender, e):
